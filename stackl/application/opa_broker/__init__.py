0cc827d6d5d497d96fe2ea0b1432c1ab69bdd91f
SvenAkk <sven.akkermans@outlook.be>
2020-02-19 10:54:59 +0100

    Added data loading (WIP)

--- stackl/application/opa_broker/__init__.py
+++ stackl/application/opa_broker/__init__.py
@@ -18,6 +18,7 @@ class OPABroker():
 
         self.opa_host = "http://{}".format(get_config_key("OPA_HOST"))
         self.load_default_policies()
+        self.load_default_data()
 
     def load_default_policies(self):
         logger.debug("[OPABroker] load_default_policies.")
@@ -25,16 +26,22 @@ class OPABroker():
         response = requests.put(self.opa_host + "/v1/policies/example", data=data)
         logger.debug("[OPABroker] load_default_policies. Response:{}".format(response))
 
+    def load_default_data(self):
+        logger.debug("[OPABroker] load_default_data.")
+        data = self.create_opa_input_data()
+        self.load_opa_data(data, "default")
 
-    def create_opa_input(self, input):
-        input_dict = {  # create input to hand to OPA
-            "input": {
-                "user": "test",
-                "path": "test",  
-                "method": "test" 
-            }
+    def create_opa_input_data(self, input=None):
+        if input:
+            return input
+        else:
+            return {  # create input to hand to OPA
+                "input": {
+                    "user": "test",
+                    "path": "test",  
+                    "method": "test2" 
+                }
         }
-        return input_dict
 
     def ask_opa_policy_decision(self):
         # rsp = requests.post
@@ -48,7 +55,19 @@ class OPABroker():
 
     def get_opa_policies(self):
         logger.debug("[OPABroker] get_opa_policies.")
-        response = requests.get(self.opa_host + "/v1/policies", verify=False)
+        response = requests.get(self.opa_host + "/v1/policies")
         result = response.json()
         logger.debug("[OPABroker] get_opa_policies. Result: {}".format(result))
         return result
+
+    def get_opa_data(self, path = "current"):
+        logger.debug("[OPABroker] get_opa_data for path '{}'".format(path))
+        response = requests.get(self.opa_host + "/v1/data/"  + path)
+        result = response.json()
+        logger.debug("[OPABroker] get_opa_data. Result: {}".format(result))
+        return result
+
+    def load_opa_data(self, data, path="current"):
+        logger.debug("[OPABroker] load_opa_data for data '{0}' and path '{1}'".format(data, path))
+        response = requests.put(self.opa_host + "/v1/data/" + path, data=json.dumps(data))
+        logger.debug("[OPABroker] load_opa_data. response: {}".format(response))
