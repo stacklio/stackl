be91a8fe7243f6d98f1e35cce4fc3e9d772cab0b
SvenAkk <sven.akkermans@outlook.be>
2020-02-19 10:09:49 +0100

    loading of default policy in opa

--- stackl/application/opa_broker/__init__.py
+++ stackl/application/opa_broker/__init__.py
@@ -1,23 +1,29 @@
 import json
 import requests
+import os
 
 import globals
 import logging
 from utils.general_utils import get_config_key
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("STACKL_LOGGER")
 
-class OPABroker:
+class OPABroker():
 
     def __init__(self):
+        pass
+
+    def start(self):
+        logger.debug("[OPABroker] Initialising OPABroker.")
+
         self.opa_host = "http://{}".format(get_config_key("OPA_HOST"))
         self.load_default_policies()
 
     def load_default_policies(self):
-        logger.info("[OPABroker] load_default_policies.")
-        # data = open('stackl_default_policies.rego', 'rb').read()
-        # response = requests.put('http://localhost:8181/v1/policies/example', data=data)
-        # logger.debug("[OPABroker] load_default_policies. Response:{}".format(response))
+        logger.debug("[OPABroker] load_default_policies.")
+        data = open('/app/opa_broker/stackl_default_policies.rego', 'rb').read()
+        response = requests.put(self.opa_host + "/v1/policies/example", data=data)
+        logger.debug("[OPABroker] load_default_policies. Response:{}".format(response))
 
 
     def create_opa_input(self, input):
@@ -41,7 +47,7 @@ class OPABroker:
         pass
 
     def get_opa_policies(self):
-        logger.info("[OPABroker] get_opa_policies.")
+        logger.debug("[OPABroker] get_opa_policies.")
         response = requests.get(self.opa_host + "/v1/policies", verify=False)
         result = response.json()
         logger.debug("[OPABroker] get_opa_policies. Result: {}".format(result))
