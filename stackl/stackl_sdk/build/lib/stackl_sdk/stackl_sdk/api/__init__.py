aa06e8e54fb96f42d48bddb859173d551ee31fe3
SvenAkk <sven.akkermans@outlook.be>
2020-03-10 12:19:34 +0100

    merged master into opa_poc_dev

---
+++ stackl/stackl_sdk/build/lib/stackl_sdk/stackl_sdk/api/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from stackl_sdk.api.documents_api import DocumentsApi
+from stackl_sdk.api.functional_requirements_api import FunctionalRequirementsApi
+from stackl_sdk.api.services_api import ServicesApi
+from stackl_sdk.api.stack_application_templates_api import StackApplicationTemplatesApi
+from stackl_sdk.api.stack_infrastructure_templates_api import StackInfrastructureTemplatesApi
+from stackl_sdk.api.stacks_api import StacksApi
