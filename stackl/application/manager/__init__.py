0ba914d710a7f0a4a1df12bf814ddfe589f57a20
SvenAkk <sven.akkermans@outlook.be>
2020-03-16 08:56:45 +0100

    added more todos, small cleanup and modified .gitignore to include some parts of the stackl build

--- stackl/application/manager/__init__.py
+++ stackl/application/manager/__init__.py
@@ -2,17 +2,8 @@ from abc import ABC
 
 from datastore.datastore_factory import DataStoreFactory
 
-
 class Manager(ABC):
 
     def __init__(self, manager_factory):
-        self.version = self.__get_version()
         self.store = DataStoreFactory().get_store()
         self.manager_factory = manager_factory
-
-    def __get_version(self):
-        try:
-            with open(os.path.join('/etc/stackl_src/', 'VERSION')) as version_file:
-                return version_file.read().strip()
-        except:
-            return 'v0.0.00000'
