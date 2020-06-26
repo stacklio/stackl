2c65b1a42c77cdc325e4e996169df1c677d236b5
Frederic <frederic.vanreet@icloud.com>
2020-06-26 13:25:52 +0200

    Add functionality to easily make snapshots
    We can now create snapshots through the cli with the commands
    stackl get snapshots <type> <name>
    stackl create snapshot <type> <name>
    stackl get snapshot <snapshot_name>
    stackl restore <snapshot_name>
    stackl delete <snapshot_name>
    When restoring a stack_instance snapshot, an agenttask will also
    be created to update the stack instance so the state of the stack instance
    is actually updated

--- library/stackl/stackl/tasks/snapshot_task.py
+++ library/stackl/stackl/tasks/snapshot_task.py
@@ -7,13 +7,14 @@ class SnapshotTask(Task):
     topic: str = "snapshot_task"
     snapshot_doc_type: str = None
     snapshot_doc_name: str = None
+    subtype: str = None
 
     @validator('subtype')
     def valid_subtypes(cls, subtype):
         subtypes = [
-            "GET_SNAPSHOT", "LIST_SNAPSHOT", "CREATE_SNAPSHOT",
+            "GET_SNAPSHOT", "GET_SNAPSHOTS", "CREATE_SNAPSHOT",
             "RESTORE_SNAPSHOT", "DELETE_SNAPSHOT"
         ]
         if subtype not in subtypes:
-            raise ValueError("subtype for DocumentTask not valid")
+            raise ValueError("subtype for SnapshotTask not valid")
         return subtype
