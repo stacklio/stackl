a6a0c1348987658006882f686be34a37493ebdb2
Frederic <frederic.vanreet@icloud.com>
2020-07-14 14:16:25 +0200

    Add Dockerfile

--- stackl/cli/setup.py
+++ stackl/cli/setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='stackl-cli',
-    version='0.1.3dev',
+    version='0.2.0dev',
     py_modules=['stackl', 'commands', 'context'],
     packages=find_packages(),
     install_requires=[
-        'stackl-client==0.1.3dev', 'pyYAML==5.3', 'Click==7.0',
+        'stackl-client==0.2.0dev', 'pyYAML==5.3', 'Click==7.0',
         'mergedeep==1.3.0', 'tabulate==0.8.6', 'glom==19.10.0'
     ],
     entry_points='''
