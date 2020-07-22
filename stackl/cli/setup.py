05bee716bb44f615d96e0045b902ca017aa54b5a
Frederic <frederic.vanreet@icloud.com>
2020-07-22 18:58:29 +0200

    fix get all instances

--- stackl/cli/setup.py
+++ stackl/cli/setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='stackl-cli',
-    version='0.2.0dev',
+    version='0.2.0rc1',
     py_modules=['stackl', 'commands', 'context'],
     packages=find_packages(),
     install_requires=[
-        'stackl-client==0.2.0dev', 'pyYAML==5.3', 'Click==7.0',
+        'stackl-client==0.2.0rc2', 'pyYAML==5.3', 'Click==7.0',
         'mergedeep==1.3.0', 'tabulate==0.8.6', 'glom==19.10.0'
     ],
     entry_points='''
