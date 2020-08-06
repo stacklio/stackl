17e4db03761a09f3c1c6d798961a58541ba9b47f
Frederic <frederic.vanreet@icloud.com>
2020-08-06 13:54:43 +0200

    Add option to use multiple params for cli

--- stackl/cli/commands/update.py
+++ stackl/cli/commands/update.py
@@ -12,16 +12,19 @@ def update():
 
 
 @update.command()
-@click.option('-p', '--params', default="{}")
+@click.option('-p', '--params', default="[{}]", multiple=True)
 @click.option('-s', '--secrets', default="{}")
 @click.option('-d', '--disable-invocation', is_flag=True)
 @click.argument('instance-name')
 @pass_stackl_context
 def instance(stackl_context: StacklContext, params, secrets,
              disable_invocation, instance_name):
+    final_params = {}
+    for item in params:
+        final_params = {**final_params, **json.loads(item)}
     invocation = stackl_client.StackInstanceUpdate(
         stack_instance_name=instance_name,
-        params=json.loads(params),
+        params=final_params,
         secrets=json.loads(secrets))
     if disable_invocation:
         invocation.disable_invocation = True
