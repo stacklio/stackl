56dd7a17e7c3376d138defaf588d4a19f098d7fe
Frederic <frederic.vanreet@icloud.com>
2020-08-25 08:24:47 +0200

    Release 0.2.1

--- stackl/core/core/main.py
+++ stackl/core/core/main.py
@@ -17,7 +17,7 @@ logger.info(
 app = FastAPI(
     title="STACKL",
     description="stackl",
-    version="0.2.1dev"
+    version="0.2.1"
 )
 
 app.include_router(infrastructure_base_router.router,
