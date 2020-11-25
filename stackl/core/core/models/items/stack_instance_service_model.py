963d5ac53f3551c4c6b19076e426d45eead56b84
Frederic <frederic.vanreet@icloud.com>
2020-11-25 15:46:00 +0100

    Add counter for increasing hostname

--- stackl/core/core/models/items/stack_instance_service_model.py
+++ stackl/core/core/models/items/stack_instance_service_model.py
@@ -1,11 +1,18 @@
 """
 StackInstanceService model
 """
-from typing import Dict, Any, List
+import re
+from typing import Any, Dict, List
 
-from pydantic import BaseModel  # pylint: disable=E0611 #error in pylint
+from pydantic import BaseModel
+from redis import Redis
 
+from core import config
 
+# TODO get_redis in the future? doesnt work for now
+redis =  Redis(host=config.settings.stackl_redis_host,
+                      port=config.settings.stackl_redis_port,
+                      password=config.settings.stackl_redis_password)
 class StackInstanceService(BaseModel):
     """
     StackInstanceService model
@@ -16,6 +23,7 @@ class StackInstanceService(BaseModel):
     secrets: Dict[str, Any] = None
     hosts: List[str] = None
     resources: Dict[str, str] = None
+    service: str = None
     agent: str = ""
     opa_outputs: Dict[str, Any] = {}
     outputs: Dict[str, Any] = {}
@@ -27,16 +35,27 @@ class StackInstanceService(BaseModel):
         # Clear previous machine names
         self.hosts = []
         self.provisioning_parameters['machine_names'] = []
+        counter_match = re.match(r'.*{ *counter\((\w+) *, *(\d+) *\) *}.*', stackl_hostname)
+        if counter_match:
+            variable_name = counter_match.group(1)
+            default_counter = int(counter_match.group(2))
+            if redis.get(variable_name):
+                value = redis.incr(variable_name)
+            else:
+                value = redis.incr(variable_name, default_counter)
+            stackl_hostname = re.sub(r'{ *counter\(\w+ *, \d+ *\) *}', str(value), stackl_hostname)
         if instances:
             for i in range(self.provisioning_parameters["instances"]):
                 replaced_hostname = stackl_hostname \
                     .replace('{ri}', "{:02d}".format(infra_target_counter)) \
                     .replace('{hi}', "{:02d}".format(i + 1))
                 self.hosts.append(replaced_hostname)
-                self.provisioning_parameters['machine_names'].append(replaced_hostname)
+                self.provisioning_parameters['machine_names'].append(
+                    replaced_hostname)
         else:
             replaced_hostname = stackl_hostname \
                 .replace('{ri}', "{:02d}".format(infra_target_counter)) \
                 .replace('{hi}', "01")
             self.hosts.append(replaced_hostname)
-            self.provisioning_parameters['machine_names'].append(replaced_hostname)
+            self.provisioning_parameters['machine_names'].append(
+                replaced_hostname)
