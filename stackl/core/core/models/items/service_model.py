9d48a18bb67b2d6e37d78198e1adad79b6696243
Frederic <frederic.vanreet@icloud.com>
2021-06-23 17:06:25 +0200

    Fix deleting by not spinning up an outputs container

--- stackl/core/core/models/items/service_model.py
+++ stackl/core/core/models/items/service_model.py
@@ -4,8 +4,12 @@ Service model
 from typing import List, Dict, Any
 
 from core.models.configs.document_model import BaseDocument
+from pydantic import BaseModel  # pylint: disable=E0611 #error in pylin
 
 
+class ServicePolicyDescription(BaseModel):
+    name: str
+    inputs: Dict[str, str] = {}
 class Service(BaseDocument):
     """
     Service model
@@ -15,4 +19,5 @@ class Service(BaseDocument):
     name: str
     functional_requirements: List[str]
     resource_requirements: Dict[str, Any] = {}
-    service_policies: List[Dict[str, Any]] = []
+    service_policies: List[ServicePolicyDescription] = []
+
