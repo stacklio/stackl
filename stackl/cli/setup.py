d6c42ce1a130f738fee0187690173266c8e63dae
Stef Graces <stefgraces@hotmail.com>
2021-07-08 12:25:36 +0200

    Change versions to dev version

--- stackl/cli/setup.py
+++ stackl/cli/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-__version__ = "0.5.0"
+__version__ = "0.5.1dev"
 
 setup(
     name='stackl-cli',
@@ -8,7 +8,7 @@ setup(
     py_modules=['stackl', 'commands', 'context'],
     packages=find_packages(),
     install_requires=[
-        f'stackl-client==0.5.0', 'pyYAML==5.4.1', 'Click==7.0',
+        f'stackl-client==0.5.1dev', 'pyYAML==5.4.1', 'Click==7.0',
         'mergedeep==1.3.0', 'tabulate==0.8.6', 'glom==19.10.0'
     ],
     entry_points='''
